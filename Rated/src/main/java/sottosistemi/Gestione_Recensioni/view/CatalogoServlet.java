ce6ea3d82aa3f3e215f6efad91a32978ddcdf80c
superfra01 <151184247+superfra01@users.noreply.github.com>
2025-01-11 20:31:58 +0100

    catalogo servlet

--- Rated/src/main/java/sottosistemi/Gestione_Recensioni/view/CatalogoServlet.java
+++ Rated/src/main/java/sottosistemi/Gestione_Recensioni/view/CatalogoServlet.java
@@ -1,13 +1,9 @@
 package sottosistemi.Gestione_Recensioni.view;
 
 
+import model.Entity.FilmBean;
+import sottosistemi.Gestione_Film.service.CatalogoService;
 
-import model.Entity.UtenteBean;
-import model.Entity.RecensioneBean;
-import sottosistemi.Gestione_Utenti.service.AutenticationService;
-import sottosistemi.Gestione_Utenti.service.ProfileService;
-import sottosistemi.Gestione_Recensioni.service.RecensioniService;
-import utilities.FieldValidator;
 
 import java.io.IOException;
 import java.util.List;
@@ -22,66 +18,29 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/profile")
 public class CatalogoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private ProfileService ProfileService;
+	private CatalogoService CatalogoService;
 
     @Override
     public void init() {
-        ProfileService = new ProfileService();
+        CatalogoService = new CatalogoService();
     }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	HttpSession session = request.getSession(true);
-    	UtenteBean user = (UtenteBean) session.getAttribute("user");
-        if(user!=null) {
-        	
-        	RecensioniService RecensoniService = new RecensioniService();
-        	List<RecensioneBean> recensioni = RecensoniService.FindRecensioni(user.getEmail());
-        	session.setAttribute("recensioni", recensioni);
-        	request.getRequestDispatcher("/WEB-INF/jsp/profile.jsp").forward(request, response);	
-        }else {
-        	response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            response.getWriter().write("You can't access the profile page if you are not autenticated");
-        }
+
+    	List<FilmBean> films = CatalogoService.getFilms();
+    	session.setAttribute("films", films);
+    	
+        
+        request.getRequestDispatcher("/WEB-INF/jsp/catalogo.jsp").forward(request, response);
         
         
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	if(request.getParameter("operationType").equals("ProfileModify")) {
-    		String username = request.getParameter("username");
-            String email = request.getParameter("email");
-            String password = request.getParameter("password");
-            String biography = request.getParameter("biography");
-            byte[] icon = request.getParameter("icon").getBytes();
-
-            if (FieldValidator.validateUsername(username) &&
-                FieldValidator.validatePassword(password)) {
-            	
-            	UtenteBean utente = ProfileService.ProfileUpdate(username, email, password, biography, icon);
-            	
-            	HttpSession session = request.getSession(true);
-            	session.setAttribute("user", utente);
-            	
-                response.sendRedirect(request.getContextPath() + "/profile.jsp"); // Redirect to login after successful registration
-               
-            } else {
-                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                response.getWriter().write("Invalid form data. Check your inputs.");
-            }
-    	}else if(request.getParameter("operationType").equals("PasswordModify")) {
-    		String email = request.getParameter("email");
-            String password = request.getParameter("password");
-            if(FieldValidator.validatePassword(password)) {
-            	UtenteBean utente = ProfileService.PasswordUpdate(email, password);
-            	
-            	HttpSession session = request.getSession(true);
-            	session.setAttribute("user", utente);
-            	
-            	response.sendRedirect(request.getContextPath() + "/profile.jsp");
-            }
-    	}
+    	
         
     }
 }
